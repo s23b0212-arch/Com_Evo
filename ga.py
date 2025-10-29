9999b0559410e89a383ee90a52caf9099c6f28a8
s23b0212-arch <s23b0212@siswa.umk.edu.my>
2025-10-29 17:50:07 +0800

    Update ga.py

--- ga.py
+++ ga.py
@@ -123,3 +123,5 @@ def main(POP_SIZE, MUT_RATE, TARGET, GENES):
         break
       print('String: ' + str(population[0][0]) + ' Generation: ' + str(generation) + ' Fitness: ' + str(population[0][1]))
       generation+=1
+
+result = main(POP_SIZE, MUT_RATE, TARGET, GENES)
