50db7447ddcb169e8fdc1944b21d3f181edd4850
s23b0212-arch <s23b0212@siswa.umk.edu.my>
2025-10-29 18:19:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ st.set_page_config(
 #analysis = st.Page('PLO_sum.py', title='Pencapaian Akademik', icon=":material/school:")
 #overall = st.Page('Overall_PLO.py', title="Overall PLO")
 genetic = st.Page('ga.py', title='Genetic Algorithm', icon=":material/business:")
-travelling = st.Page('tsp.py', title='Travelling Salesman', icon=":material/business:")
+travelling = st.Page('tsp2.py', title='Travelling Salesman', icon=":material/business:")
 home = st.Page('main.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
